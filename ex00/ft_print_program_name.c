e9dcbc258c21887412eb77e214ee0f65599f7f9f
mittsu0 <threebridge3824@gmail.com>
2022-04-01 01:26:01 +0900

    modify

--- ex00/ft_print_program_name.c
+++ ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: unix_user <unix_user@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 23:55:49 by unix_user         #+#    #+#             */
-/*   Updated: 2022/03/31 00:24:03 by unix_user        ###   ########.fr       */
+/*   Updated: 2022/04/01 01:18:33 by unix_user        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,11 @@
 
 int	main(int argc, char *argv[])
 {
+	int	i;
+
+	i = argc;
 	while (*argv[0])
 		write(1, argv[0]++, 1);
-	write(1, &"\n", 1);
+	write(1, "\n", 1);
+	return (0);
 }
